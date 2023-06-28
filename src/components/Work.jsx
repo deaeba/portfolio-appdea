63b83a5a18371eb6f152639a0dda0cabdade84bc
deaeba <deaebanoidze@gmail.com>
2023-06-28 10:39:32 -0400

    updated work. js

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -29,7 +29,7 @@ const Work = () => {
     <div id="work" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
       <h1 className="text-4xl font-bold text-center text-[#001b5e]">
         <a
-          href="https://drive.google.com/file/d/13OluR4lJ_MWWkDuHEd4DtsZpOrMlP322/view?usp=sharing"
+          href="https://drive.google.com/file/d/1VgZA5NU-oLbqbzp_eJghokIEUFosFxnn/view?usp=sharing"
           className="inline-block bg-[#001b5e]  text-white px-4 py-2 rounded-md hover:bg-[#00163a]"
           target="_blank"
           rel="noopener noreferrer"
